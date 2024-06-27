e9f2937d565079629acfc99121d9bbb58646b824
Elysia Oh <102720577+elysiaoh@users.noreply.github.com>
2024-06-26 23:49:26 -0700

    Made major style changes

---
+++ portfolio-web/resources/js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["for fun", "with an extra shot in my latte", "with love and coffee built with HTML/CSS and JS 🫰"],
+    typeSpeed: 200,
+    backSpeed:200,
+    loop:true
+})
