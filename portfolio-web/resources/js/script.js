5762de987ab53417830d441eed8c965d26fd63cc
Elysia Oh <102720577+elysiaoh@users.noreply.github.com>
2024-06-27 01:08:47 -0700

    Removed contact page

--- portfolio-web/resources/js/script.js
+++ portfolio-web/resources/js/script.js
@@ -4,3 +4,7 @@ var typed = new Typed(".auto-type", {
     backSpeed:200,
     loop:true
 })
+
+function downloadResume(){
+    window.open("https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg");
+}
